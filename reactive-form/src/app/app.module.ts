e49428fb0838b4153e3170a20ace3543cbb481f5
wdvOps <dvops@outlook.com>
2021-05-15 00:37:34 -0300

    Update style

--- reactive-form/src/app/app.module.ts
+++ reactive-form/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './clientes/formulario/formulario.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { FormularioComponent } from './clientes/formulario/formulario.component'
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    MdbModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
